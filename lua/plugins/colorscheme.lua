80544c22571fd8b3f71f08497ebe7160bd18906a
Renell Castro <renellc9819@gmail.com>
2023-12-07 23:26:45 -0800

    Updated color scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+vim.g.material_style = "palenight"
+
+return {
+  {
+    "marko-cerovac/material.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "material",
+    },
+  },
+}
